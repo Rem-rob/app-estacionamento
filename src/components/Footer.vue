b25f369989a64a769fc56dac636db12492e244ae
Rob-rem <rem657369@hotmail.com>
2021-12-13 16:01:24 -0300

    Projeto de estacionamento do canal Leo - Coffee Tag

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    
+<p> Sistema de Estacionamento 2021 </p>
+
+</template>
+
+<script>
+
+export default {
+    name: "Footer"
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Cadastro">
+    <h1>Cadastro de veículos</h1>
+  </div>
+</template>
